275b3eb1f75bb0197babdf011684de64c9e88f37
Грачева Юлия ИПсп-121 <GrachevaIPsp121@example.com>
2025-05-23 17:29:16 +0300

    fixing servers.mjs

--- servers.mjs
+++ servers.mjs
@@ -70,7 +70,9 @@ db.serialize(() => {
     });
 
     const PORT = process.env.PORT || 3004;
-    app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+    app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
+});
 
     process.on('SIGINT', () => {
         db.close((err) => {
