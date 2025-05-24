1e22a3e0e4655fda4b37330ffc118394cde9d1e2
Грачева Юлия ИПсп-121 <GrachevaIPsp121@example.com>
2025-05-24 03:09:54 +0300

    fixing servers.mjs

--- servers.mjs
+++ servers.mjs
@@ -11,9 +11,23 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bcrypt from 'bcrypt';
 
-
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const buildPath = path.join(__dirname, 'build');
+
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
 
 dotenv.config({ path: './process.env' });
 
@@ -34,8 +48,6 @@ const corsOptions = {
     allowedHeaders: ['Content-Type'],
     credentials: true
 };
-
-const app = express();
 
 import bodyParser from "body-parser";
 
@@ -785,9 +797,3 @@ app._router.stack.forEach(r => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'scripts')));
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
