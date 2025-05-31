1bc1d3748f7589919700302725317c2a237f3c13
Грачева Юлия ИПсп-121 <GrachevaIPsp121@example.com>
2025-05-31 13:16:54 +0300

    fixing yandex search

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import App from './App.js';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals.js';
-import { SearchProvider } from "./Components/SearchContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <SearchProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </SearchProvider>
   </BrowserRouter>
 );
 
