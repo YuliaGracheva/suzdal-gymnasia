fb66267eb125e17527776bb2946407e650b1bce2
Грачева Юлия ИПсп-121 <GrachevaIPsp121@example.com>
2025-05-31 15:54:19 +0300

    fixing yandex search

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,17 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
-export default function YandexSearchResults() {
-  const resultsRef = useRef(null);
-
+export default function YandexSearchResultsEmbed() {
   useEffect(() => {
-    const initYandexResults = () => {
-      if (window.Ya?.Site?.Results?.init) {
-        window.Ya.Site.Results.init();
-      } else {
-        setTimeout(initYandexResults, 300); 
-      }
-    };
-
     if (!document.getElementById("yandex-site-results-script")) {
       const script = document.createElement("script");
       script.id = "yandex-site-results-script";
@@ -21,26 +11,32 @@ export default function YandexSearchResults() {
       script.src =
         (window.location.protocol === "https:" ? "https:" : "http:") +
         "//site.yandex.net/v2.0/js/all.js";
-      script.onload = initYandexResults;
       document.body.appendChild(script);
-    } else {
-      initYandexResults();
     }
+
+    // запускаем инициализацию
+    if (!window.yandex_site_callbacks) {
+      window.yandex_site_callbacks = [];
+    }
+
+    window.yandex_site_callbacks.push(function () {
+      if (window.Ya?.Site?.Results?.init) {
+        window.Ya.Site.Results.init();
+      }
+    });
   }, []);
 
-  const bemData = {
-    tld: "ru",
-    language: "ru",
-    encoding: "",
-    htmlcss: "1.x",
-    updatehash: true,
-  };
+  const searchHtml = `
+    <div id="ya-site-results"
+         data-bem='{
+           "tld": "ru",
+           "language": "ru",
+           "encoding": "",
+           "htmlcss": "1.x",
+           "updatehash": true
+         }'>
+    </div>
+  `;
 
-  return (
-    <div
-      id="ya-site-results"
-      ref={resultsRef}
-      data-bem={JSON.stringify(bemData)}
-    ></div>
-  );
+  return <div dangerouslySetInnerHTML={{ __html: searchHtml }} />;
 }
